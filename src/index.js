fbe2c018c6b8257c0d71ddf531c2929f545d11ac
furse2000 <furse2000@yandex.ru>
2021-03-15 19:37:35 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function reverse (n) {
-  
+if(n<0)
+n=n*(-1);
+let ff=0;
+let kk=n.toString()
+/*if(kk.length%2)
+{
+    for(let i=0;i<kk.length/2;i++)
+        {
+           ff=kk[i];
+           kk[i]=kk[kk.length-1-i] 
+           kk[kk.length-1-i]=ff;
+        }
+}else
+{
+    for(let i=0;i<(kk.length-1)/2;i++)
+    {
+        ff=kk[i];
+        kk[i]=kk[kk.length-1-i] 
+        kk[kk.length-1-i]=ff;       
+    }
+}
+let k=0;
+k=+kk;*/
+return kk.split('').reverse().join('');
 }
